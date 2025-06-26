afa720bb91b2acef4beb1e370d6f86fcfd2daf46
Sergey Korolev <sergiussmithe@gmail.com>
2025-06-26 18:27:54 +0300

    Реализовал паттерн MVVM и использовал CommunityToolkit.MVVM

---
+++ WpfUI_Test/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUI_Test.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int _testCounter;
+
+        [RelayCommand]
+        public void Test()
+        {
+            TestCounter++;
+        }
+    }
+}
--- WpfUI_Test/Views/MainWindow.xaml.cs
+++ WpfUI_Test/Views/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
 ﻿using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfUI_Test
 {
@@ -17,7 +9,7 @@ namespace WpfUI_Test
     public partial class MainWindow : Window
     {
         public MainWindow()
-        {
+        {   
             InitializeComponent();
         }
     }
